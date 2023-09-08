b4fa59eeebd09c5dc96dc4e1734e5aa3743ca13d
Chiang Jou <tiffanyj.chiang@gmail.com>
2023-09-08 16:43:21 +0800

    feat: add day.js

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express')
-const handlebars = require('express-handlebars')
+const exphbs = require('express-handlebars')
 const helpers = require('./_helpers')
+const flash = require('connect-flash')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.engine('hbs', handlebars({ extname: '.hbs' }))
+// handlebars
+app.engine('hbs', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  helpers: handlebarsHelpers
+}))
 app.set('view engine', 'hbs')
+
+// flash
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.signinUser = helpers.getUser(req)
+  next()
+})
 
 app.use(routes)
 // use helpers.getUser(req) to replace req.user
