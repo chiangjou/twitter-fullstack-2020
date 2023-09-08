6ed655e7340a83429dba9b02923607152c8fee0d
Chiang Jou <tiffanyj.chiang@gmail.com>
2023-09-08 15:56:37 +0800

    feat: add user signup page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const tweetController = require('../controllers/tweet-controller')
-
+const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin')
 router.use('/admin', admin)
+
+// User Signup
+router.get('/signup', userController.signupPage)
+router.post('/signup', userController.signup)
 
 router.get('/tweets', tweetController.getTweets)
 
