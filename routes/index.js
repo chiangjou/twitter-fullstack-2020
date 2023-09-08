37cca03a7cb8199fc8819d9bd4313865b0dc1d00
Chiang Jou <tiffanyj.chiang@gmail.com>
2023-09-08 11:21:13 +0800

    feat: add admin tweets page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const admin = require('./modules/admin')
+router.use('/admin', admin)
 module.exports = router
